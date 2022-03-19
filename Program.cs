29f185e37f057c5e81e5191e967153142fd529fd
Rahmat <rahmar.uafu@gmail.com>
2022-03-19 17:00:56 +0530

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+namespace Assignment_Asynchronous
+{
+    public class Program
+    {
+        static async Task <string[]> Read(string Filename)
+        {
+            var ReadData = await File.ReadAllLinesAsync(Filename);
+            return ReadData; 
+        }
+        static async Task Write(string[] WriteData,string Filename)
+        {
+            await File.WriteAllLinesAsync(Filename, WriteData);
+            System.Console.WriteLine("File A.txt content coppied in File B.txt");
+        }
+        public static async Task CopyContent()
+        {
+            var Content = Read("A.txt");
+            await Write(Content.Result,"B.txt");
+        }
+        public static void Main(string[] args)
+        {
+            CopyContent();
+        }        
+    }
+}
